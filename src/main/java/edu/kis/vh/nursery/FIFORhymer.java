649596f6b5d61e320535df0836c0d6a73d4fcc8f
BaDaQu <bartek.raj2@gmail.com>
2025-04-01 16:53:03 +0200

    17. Uzupełnienie dokumentacji Javadoc dla FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,18 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa reprezentująca wyliczankę działającą na zasadzie FIFO (First In, First Out).
+ * Elementy są wyliczane w kolejności, w jakiej zostały dodane.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Wylicza element z wyliczanki zgodnie z zasadą FIFO.
+     *
+     * @return Wyliczony element z wyliczanki. Zwraca -1, jeśli wyliczanka jest pusta.
+     */
     @Override
     public int countOut() {
         while (!callCheck())
